ab446b08d22488b80df4cb83afbb620786416ea9
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-07-13 17:07:18 +0800

    增加dnssec验证方式

--- go-web/src/DNSQuery/DNSSec.go
+++ go-web/src/DNSQuery/DNSSec.go
@@ -3,21 +3,28 @@ package DNSQuery
 import (
 	"strconv"
 	// "fmt"
+	"net"
 	"github.com/miekg/dns"
+	"time"
 )
 
 func GetDNSKEY(domain string, authenticationServer string, port int) ([]*dns.DNSKEY, []*dns.RRSIG, error) {
 	queryServer := authenticationServer + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout("tcp", queryServer, time.Duration(5)*time.Second)
+	if err != nil {
+		return nil, nil,err
+	}
 	m := new(dns.Msg)
 	m.SetEdns0(4096, true)
 	m.Id = dns.Id()
 	m.RecursionDesired = false
 	m.Question = make([]dns.Question, 1)
-	m.Question[0] = dns.Question{domain, dns.TypeDNSKEY, dns.ClassINET}
-	c := new(dns.Client)
-	in, _, err := c.Exchange(m, queryServer)
+	m.Question[0] = dns.Question{dns.Fqdn(domain), dns.TypeDNSKEY, dns.ClassINET}
+	// c := new(dns.Client)
+	// c := new(dns.Client)
+	in, err := dns.ExchangeConn(conn, m)
 	if err != nil {
-		return []*dns.DNSKEY{}, []*dns.RRSIG{}, err
+		return  nil, nil, err
 	}
 	keyList := []*dns.DNSKEY{}
 	rrsigList := []*dns.RRSIG{}
@@ -36,18 +43,60 @@ func GetDNSKEY(domain string, authenticationServer string, port int) ([]*dns.DNS
 
 }
 
+func GetDS(domain string, authenticationServer string, port int) ([]*dns.DS, []*dns.RRSIG, error) {
+	queryServer := authenticationServer + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout("tcp", queryServer, time.Duration(5)*time.Second)
+	if err != nil {
+		return nil, nil,err
+	}
+	defer conn.Close()
+	m := new(dns.Msg)
+	m.SetEdns0(4096, true)
+	m.Id = dns.Id()
+	m.RecursionDesired = false
+	m.Question = make([]dns.Question, 1)
+	m.Question[0] = dns.Question{dns.Fqdn(domain), dns.TypeDS, dns.ClassINET}
+	// c := new(dns.Client)
+	in, err := dns.ExchangeConn(conn, m)
+	if err != nil {
+		return  nil, nil, err
+	}
+	DSList := []*dns.DS{}
+	rrsigList := []*dns.RRSIG{}
+	for _, answer := range in.Answer {
+		switch t := answer.(type) {
+		case *dns.DS:
+			DSList = append(DSList, t)
+		case *dns.RRSIG:
+			rrsigList = append(rrsigList, t)
+		default:
+
+		}
+	}
+
+	return DSList, rrsigList, nil
+
+}
+
+
 
 func DNSSecDomainRRSig(domain string, authenticationServer string, port int) ([]*dns.RR,[]*dns.RRSIG, error){
 	queryServer := authenticationServer + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout("tcp", queryServer, time.Duration(5)*time.Second)
+	if err != nil {
+		return nil, nil,err
+	}
+	defer conn.Close()
+
 	m := new(dns.Msg)
 	m.SetEdns0(4096, true)
 	m.Id = dns.Id()
 	m.Question = make([]dns.Question, 1)
 	m.Question[0] = dns.Question{dns.Fqdn(domain), dns.TypeA, dns.ClassINET}
-	c := new(dns.Client)
-	in, _, err := c.Exchange(m, queryServer)
+	// c := new(dns.Client)
+	in, err := dns.ExchangeConn(conn, m)
 	if err != nil {
-		return  []*dns.RR{},[]*dns.RRSIG{}, err
+		return  nil, nil, err
 	}
 	rr_list:= []*dns.RR{}
 	rrsigList := []*dns.RRSIG{}
--- go-web/src/Process/dnssec.go
+++ go-web/src/Process/dnssec.go
@@ -4,6 +4,7 @@ import (
 	"DNSQuery"
 	"fmt"
 	"github.com/miekg/dns"
+	"strings"
 )
 var root_key = make(map[string]string,2)
 
@@ -11,79 +12,190 @@ func GetRootKey(){
 	// 获取根服务器的公钥证书(存储在数组指针中)
 	rootDNSKeyList, rootRRSIGList, err := DNSQuery.GetDNSKEY(".", DNSQuery.RootServer, DNSQuery.Port)
 	if err != nil {
-		fmt.Printf("[x]暂时无法获取根证书:%v", err)
+		fmt.Printf("[x]暂时无法获取根证书:%v\n", err)
 		return
 	}
 	root_key["KSK"]=(*rootDNSKeyList[0]).PublicKey
     root_key["ZSK"]=(*rootDNSKeyList[1]).PublicKey
-	fmt.Printf("[*]已获取根证书 %d 份及签名文件 %d 份", len(rootDNSKeyList), len(rootRRSIGList))
+	fmt.Printf("[*]已获取根证书 %d 份及签名文件 %d 份\n", len(rootDNSKeyList), len(rootRRSIGList))
 
 }
 
-// func GetDNSKey(domain string) (map[string]string,error){
-// 	// 获取根服务器的公钥证书(存储在数组指针中)
-// 	domain_key := make(map[string]string,2)
-// 	DNSKeyList, RRSIGList, err := DNSQuery.GetDNSKEY(domain, DNSQuery.RecursiveServer, DNSQuery.Port)
-// 	if err != nil {
-// 		fmt.Printf("[x]暂时无法获取%v的签名秘钥:%v",domain,err)
-// 		return domain_key,err
-// 	}
-// 	fmt.Printf("%v",DNSKeyList)
-
-// 	// domain_key["KSK"]=(*DNSKeyList[0]).PublicKey
-//  //    domain_key["ZSK"]=(*DNSKeyList[1]).PublicKey
-// 	fmt.Printf("[*]已获取根证书 %d 份及签名文件 %d 份", len(DNSKeyList),len(RRSIGList))
-// 	return domain_key,nil
-// }
-
-
-func GetDomainRRSig(domain string){
+func CheckDomainRRSig(domain string){
 	// // 获取域名的公钥证书和rrsig(存储在数组指针中)
+	fmt.Printf("\n-------------DomainRRSIGCheck:.-----------\n\n",)
 	rr_list,rrsig_list,err:=DNSQuery.DNSSecDomainRRSig(domain,DNSQuery.RecursiveServer, DNSQuery.Port)
 	if err != nil {
 		fmt.Printf("[x]暂时无法获取证书和签名:%v\n", err)
 		return
 	}
-	// fmt.Printf("%+v\n",*rrsig_list[0])
-	// fmt.Printf("%+v\n",*rr_list[0])
-	fmt.Printf("[*]获取域名对应的签名文件 %d 份\n", len(rrsig_list))
+	fmt.Printf("[*]获取域名%s对应的 RR %d份和 RRSIG %d份\n",domain,len(rr_list),len(rrsig_list))
+	lenOfRRSig:=len(rrsig_list)
+	if lenOfRRSig==0{
+		fmt.Printf("[x]域名%s不包含RRSIG记录无法验证DNSSEC\n", domain)
+		return
+	}
+	for i,rrsig_item:=range(rrsig_list){
+				// 获得key_tag和signer的字符串
+		domain_rrsig_key_tag:=(*rrsig_item).KeyTag
+		domain_rrsig_key_signer:=(*rrsig_item).SignerName
 
-	// 获得key_tag和signer的字符串
-	domain_rrsig_key_tag:=(*rrsig_list[0]).KeyTag
-	domain_rrsig_key_signer:=(*rrsig_list[0]).SignerName
+	    fmt.Printf("[*]域名%v的rrset:\n\t%+v\n\tkey_tag = %v 和　签名机构　key_signer = %s\n",domain,*rr_list[i],domain_rrsig_key_tag,domain_rrsig_key_signer)
+	    // 获得对应的key
+	    // 获得ns服务器地址，带入上述的函数中获得key和rrsig
 
-    fmt.Printf("[*]域名%v的签名key_tag = %v 和　签名机构　key_signer = %s\n",domain,domain_rrsig_key_tag,domain_rrsig_key_signer)
-    // 获得对应的key
-    // 获得ns服务器地址，带入上述的函数中获得key和rrsig
-    // var signed_key string
-    domain_key_list,domain_rrsig_list,err:=DNSQuery.GetDNSKEY(domain_rrsig_key_signer,"b.iana-servers.net.", DNSQuery.Port)
-    for _,key:=range(domain_key_list){
-    	if key.KeyTag()==domain_rrsig_key_tag{
-    		fmt.Printf("[*]找到签名的秘钥%v:%v\n",domain_rrsig_key_tag,key)
-    		if err:=(rrsig_list[0]).Verify(key,[]dns.RR{*rr_list[0]});err!=nil{
-    			fmt.Println("验证失败")
-    		}else{
-    			fmt.Println("验证成功")
+	    domain_rrsig_key_signer_ns,err:=DNSQuery.ProcessGetNSList(domain_rrsig_key_signer)
+	    if err!=nil || len(domain_rrsig_key_signer_ns)==0{
+	    	fmt.Printf("[x]暂时无法获得签名者%v的ns记录:%v\n", domain_rrsig_key_signer,err)
+	    	return
+	    }
+	    fmt.Printf("[*]获得签名者%v的ns记录:\n\t%v\n", domain_rrsig_key_signer,domain_rrsig_key_signer_ns)
+	    domain_key_list,_,err:=DNSQuery.GetDNSKEY(domain_rrsig_key_signer,domain_rrsig_key_signer_ns[0], DNSQuery.Port)
+	    for _,key:=range(domain_key_list){
+	    	if key.KeyTag()==domain_rrsig_key_tag{
+	    		fmt.Printf("[*]从域名服务器%s获得签名的密钥\n\tkey_tag = %v:%v\n",domain_rrsig_key_signer_ns[0],domain_rrsig_key_tag,key)
+	    		if err:=(rrsig_list[i]).Verify(key,[]dns.RR{*rr_list[i]});err!=nil{
+	    			fmt.Println("验证失败")
+	    			return
+	    		}else{
+	    			fmt.Printf("[*]验证%s的RRSet签名成功",domain)
+	    			break
+	    		}
+	    	}
+	    }
+	}
+
+	for{
+
+		if len(domain)>1{
+			fmt.Printf("\n-------------TrustChainCheck:%s-----------\n\n",domain)
+			CheckTrustChain(domain);
+			domain=strings.Join(strings.Split(domain,".")[1:],".");
+
+
+		}else{
+			break;
+		}
+	}
+	// fmt.Printf("\n-------------TrustChainCheck:.-----------\n\n",)
+	// fmt.Printf("Finish at :%s",domain)
+
+
+
+}
+func CheckTrustChain(domain string){
+    fmt.Printf("[*]开启域名%s的验证\n",domain);
+	// 获得ns服务器地址,直接向服务器请求dnskey记录
+
+	domain_ns_list,err:=DNSQuery.ProcessGetNSList(domain)
+    if err!=nil || len(domain_ns_list)==0{
+    	fmt.Printf("[x]暂时无法获得签名者%v的ns记录:%v\n", domain_ns_list,err)
+    	return
+    }
+    fmt.Printf("[*]获得签名者%s的ns记录:%v\n", domain,domain_ns_list)
+    domain_key_list,domain_key_rrsig_list,err:=DNSQuery.GetDNSKEY(domain,domain_ns_list[0], DNSQuery.Port)
+
+    // 查询返回的key 包含zsk和ksk记录
+	fmt.Printf("[*]验证DNSKEY的合法性:获得域名%s的DNSKEY RRSet:%d,RRSIG:%d\n",domain,len(domain_key_list),len(domain_key_rrsig_list))
+    for _,rrsig_item:=range(domain_key_rrsig_list){
+    	// 获得rrsig的key_tag
+    	key_tag:=rrsig_item.KeyTag
+    	for _,key_item:=range(domain_key_list){
+    		if (*key_item).KeyTag()==key_tag{
+    			fmt.Printf("[*]已找到签名DNSKEY的密钥记录%d\n",key_tag)
+    			tempRRList:=[]dns.RR{}
+    			for _,item:=range(domain_key_list){
+    				tempRRList=append(tempRRList,item)
+    			}
+    			if err:=rrsig_item.Verify(key_item,(tempRRList));err!=nil{
+    				fmt.Println("[×]验证dnskey失败")
+    				return
+    			}else{
+    				fmt.Println("[×]验证dnskey成功")
+    				goto LABEL_KSK_CHECK_FINISH
+    			}
     		}
-
     	}
     }
 
+LABEL_KSK_CHECK_FINISH:
+
+    // 获取域名的DS记录
 
 
-    if err!=nil{
-    	fmt.Printf("[x]暂时无法获取证书和签名:%v\n", err)
-		return
+    up_level_domain:=strings.Join(strings.Split(domain,".")[1:],".");
+    fmt.Printf("[*]从父区%s获取域名%s的DS记录\n",up_level_domain,domain);
+
+    domain_ns,err:=DNSQuery.ProcessGetNSList(domain)
+    if err!=nil || len(domain_ns)==0{
+    	fmt.Printf("[x]暂时无法获得%v的ns记录:%v\n", domain,err)
+    	return
     }
-    fmt.Printf("[*]获取域名对应的签名:%v\n%v\n",domain_key_list,domain_rrsig_list)
-}
+	fmt.Printf("[*]已获得%v的ns记录%v\n",domain,domain_ns);
 
+    // 获取上层域名的ns服务器
+  	up_level_domain_ns,err:=DNSQuery.ProcessGetNSList(up_level_domain)
+    if err!=nil || len(up_level_domain_ns)==0{
+    	fmt.Printf("[x]暂时无法获得%v的ns记录:%v\n", up_level_domain,err)
+    	return
+    }
+	fmt.Printf("[*]已获得父区域%s的ns记录%v\n",up_level_domain,up_level_domain_ns);
+    domain_ds_list,domain_ds_rrsig_list,err:=DNSQuery.GetDS(domain,up_level_domain_ns[0], DNSQuery.Port)
+    if err!=nil{
+    	fmt.Printf("[x]暂时无法获得%s的DS记录:%v\n", domain,err)
+    	return
+    }
+    fmt.Printf("[*]从父区域获得%s的DS记录:%d条,rrsig记录%v条\n",domain,len(domain_ds_list),len(domain_ds_rrsig_list))
+
+    // 验证ds是否一致
+    check_ds_with_ksk:=0
+    for _,key:=range(domain_key_list){
+    	if key.Flags==257{
+    		for _,ds := range(domain_ds_list){
+    			for (key.ToDS(ds.DigestType)).Digest==ds.Digest{
+    				fmt.Printf("[*]%s ID=%d 的KSK哈希值与父区域保存的DS%d记录比对成功\n",domain,key.KeyTag(),ds.KeyTag)
+    				check_ds_with_ksk++
+    				goto LABEL_DS_CHECK_CONSISTENT;
+    			}
+    		}
+    	}
+    }
+LABEL_DS_CHECK_CONSISTENT:
+    if check_ds_with_ksk==0{
+    	fmt.Printf("[*]%s的KSK的Hash与DS记录比对失败\n",domain)
+    	return;
+    }
+
+    // 验证ds的rrsig是否已被父区域签名
+    // 获取父区域的dnskey
+	up_level_key_list,_,err:=DNSQuery.GetDNSKEY(up_level_domain,up_level_domain_ns[0], DNSQuery.Port)
+
+    for _,ds_rrsig_item:=range(domain_ds_rrsig_list){
+    	for _,dnskey_item:=range(up_level_key_list){
+    		if dnskey_item.KeyTag()==ds_rrsig_item.KeyTag{
+				tempRRList:=[]dns.RR{}
+    			for _,item:=range(domain_ds_list){
+    				tempRRList=append(tempRRList,item)
+    			}
+
+    			if err:=ds_rrsig_item.Verify(dnskey_item,tempRRList);err!=nil{
+				fmt.Printf("[×]%sDS已被父区域签名，但验证失败\n",domain)
+				return
+				}else{
+					fmt.Printf("[*]%s的DS已被父区域签名，验证成功\n",domain)
+					goto LABEL_DS_CHECK_FINISH
+				}
+    		}
+    	}
+    }
+LABEL_DS_CHECK_FINISH:
+}
 
 
 func ProcessDNSSecMain(domain string) {
 	GetRootKey()
 	// fmt.Printf("%+v\n",root_key)
-	GetDomainRRSig("www.icann.org")
+	CheckDomainRRSig("www.icann.org")
 }
 
 
